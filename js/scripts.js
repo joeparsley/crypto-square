cf5fab638f51de6f489fdba61adfc134ffdad806
Jared Eiseman and Joe Parsley <joe@>
2017-05-04 16:02:52 -0700

    add cleanArray to remove any potential undefined elements, add spacePlacer function to add a space every fifth element in the message

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,18 @@
 //BUISNESS-LOGIC
 
 var cleanText = function(input) {
-  return input.replace(/[.,\/#!$%\s@?'<|"+>\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+  return input.replace(/[.,\/#!$%@?'<|"+>\^&\*;:{}=\-_`~()]/g, "").replace(/\s/g, "").toLowerCase();
 };
+
+var cleanArray = function(input) {
+  for (var i = 0; i < input.length; i++) {
+    if (typeof input[i] == "undefined") {
+      input.splice(i,1);
+      i -= 1;
+    }
+  }
+  return input;
+}
 
 var determineSquare = function(input) {
   var square = {
@@ -30,24 +40,34 @@ var determineArray = function(square, input) {
 }
 
 //Read individual letters in the opposite direction from when they were originally pushed into a new array
-var messageArranger = function(input, square) {
+var messageArranger = function(square, input) {
   var output = [];
   for (var i = 0; i < square.cols; i++) {
     for (var j = 0; j < square.rows; j++) {
-      console.log(output);
       output.push(input[j][i]);
     }
   }
   return output;
 };
 
+//Place a space in new combined array every fifth spot
+var spacePlacer = function(input) {
+  console.log(input);
+  for (var i = 5; i < input.length; i += 5) {
+    input.splice(i, 0, " ");
+    i += 1;
+  }
+  return input;
+};
+
 var cryptoSquare = function(input){
   input = cleanText(input);
   var square = determineSquare(input);
   var output = determineArray(square, input);
-  var encrypt = messageArranger(output, square);
-  // console.log(encrypt);
-  return encrypt;
+  output = messageArranger(square, output);
+  output = cleanArray(output);
+  output = spacePlacer(output);
+  return output;
 };
 
 
